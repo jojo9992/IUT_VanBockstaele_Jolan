64bcd1d25a460cd1f69ef05afade3030ccbb0711
TABLE3 <touloniut@gmail.com>
2022-09-05 12:23:24 +0200

    TP1 timers et Led

---
+++ Embedded/projet_robot_vanbockstaele.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=projet_robot_vanbockstaele.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/projet_robot_vanbockstaele.X.production.hex
